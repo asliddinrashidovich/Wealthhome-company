948706cf0b37918c51bfe4bd61740b5f97e23538
asliddinrashidovich <asliddinnorboyev2401@gmail.com>
2024-04-18 14:55:53 +0300

    the main section is finishing up

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // main section Javascript
 
-const mainContentBox = document.getElementById('main-section-content');
+const iconHeart = document.querySelector('.icon-heart');
+const imgScale = document.querySelectorAll('img-scale');
+const imgboxOne = document.getElementById('img-scale')
+
+console.log(imgboxOne)
+
+imgboxOne.addEventListener('mouseover', (e) => {
+    iconHeart.classList.toggle('hidden')
+})
